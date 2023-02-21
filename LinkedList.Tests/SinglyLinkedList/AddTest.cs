30432b0f6c133f00e4075aff91a94cfb79f92695
Saba Gotsiridze <s.gotsiridze@altasoft.ge>
2023-02-21 12:58:07 +0400

    AddLast tests

--- LinkedList.Tests/SinglyLinkedList/AddTest.cs
+++ LinkedList.Tests/SinglyLinkedList/AddTest.cs
@@ -6,7 +6,7 @@ public class AddTest
 	[InlineData(new int[] { 1 })]
 	[InlineData(new int[] { 1, 2, 3 })]
 	[InlineData(new int[] { 0, 1, 3 })]
-	public static void Add_Values(int[] values)
+	public static void Add_First_Values(int[] values)
 	{
 		var result = new LinkedList<int>();
 		foreach (var item in values)
@@ -23,7 +23,7 @@ public class AddTest
 	[InlineData(new int[] { 1, 2, 3 })]
 	[InlineData(new int[] { 0, 1, 3 })]
 	[InlineData(new int[] { 0, 1, 3,1,2,3,4,5,6,7,87888,88,8,7,6,5,4,4444444,333333 })]
-	public static void Add_Node_Values(int[] values)
+	public static void Add_First_Node_Values(int[] values)
 	{
 		var result = new LinkedList<int>();
 		foreach (var item in values)
@@ -33,5 +33,24 @@ public class AddTest
 
 		Assert.Equal(values.Last(), result.Head?.Value);
 		Assert.Equal(values.First(), result.Tail?.Value);
+	}
+
+
+
+	[Theory]
+	[InlineData(new int[] { 1 })]
+	[InlineData(new int[] { 1, 2, 3 })]
+	[InlineData(new int[] { 0, 1, 3 })]
+	[InlineData(new int[] { 0, 1, 3, 1, 2, 3, 4, 5, 6, 7, 87888, 88, 8, 7, 6, 5, 4, 4444444, 333333 })]
+	public static void Add_Last_Node_Values(int[] values)
+	{
+		var result = new LinkedList<int>();
+		foreach (var item in values)
+			result.AddLast(new LinkedListNode<int>(item));
+
+		Assert.Equal(values.Length, result.Count);
+
+		Assert.Equal(values.Last(), result.Tail?.Value);
+		Assert.Equal(values.First(), result.Head?.Value);
 	}
 }
