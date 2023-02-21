35ba0e68404c0bd60f89aaf5b64b60906f99c6ab
Saba Gotsiridze <s.gotsiridze@altasoft.ge>
2023-02-21 13:06:35 +0400

    RemoveLast Test

---
+++ LinkedList.Tests/SinglyLinkedList/RemoveTest.cs
@@ -0,0 +1,24 @@
+﻿namespace LinkedList.Tests.SinglyLinkedList;
+
+public class RemoveTest
+{
+	[Fact]
+	public void RemoveLast_Ten_Nodes()
+	{
+		LinkedList<int> list = new LinkedList<int>();
+		for (int i = 0; i < 10; i++)
+		{
+			list.AddFirst(i);
+		}
+
+		for (int i = 10; i > 0; i--)
+		{
+			Assert.Equal(i, list.Count);
+			list.RemoveLast();
+		}
+
+		Assert.Equal(0, list.Count);
+		Assert.Null(list.Tail);
+		Assert.Null(list.Head);
+	}
+}
